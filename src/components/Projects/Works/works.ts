91a71209cea45938a58208c65d09fd2e3a3a5464
Felipe Johnson <128244805+felipejohnsonn@users.noreply.github.com>
2023-04-20 09:06:25 -0300

    Update works.ts

--- src/components/Projects/Works/works.ts
+++ src/components/Projects/Works/works.ts
@@ -1,5 +1,21 @@
 const works = [
-  
+  {
+    id: '1',
+    img: '',
+    title: '',
+    subTitle: '',
+    description: '',
+    tags: [
+      {name: "Html", icon: '' },
+      {name: "Css", icon: '' },
+      {name: "React JS", icon: '' },
+      {name: "TypeScript", icon: '' },
+      {name: "MaterialUL", icon: '' },
+      {name: "NodeJS", icon: '' },
+      {name: "MongoDB", icon: '' },
+    ],
+    github: 'Repositório Privado',
+  },
 ]
 
 export default works
