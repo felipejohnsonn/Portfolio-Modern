ad70498c2b8bf97100dd6aad164589772aa28b34
Felipe Johnson <128244805+felipejohnsonn@users.noreply.github.com>
2023-04-27 09:56:54 -0300

    Update projects.ts

--- src/components/Projects/projects.ts
+++ src/components/Projects/projects.ts
@@ -104,8 +104,8 @@ const projects = [
     description: 'Netflix landing page clone',
     tags: [
       {name: "React", icon: '/skills/react.svg' },
-      {name: "TypeScript", icon: '/skills/Typecript.svg' },
-      {name: "Vite", icon: '/skills/vite.svg' },
+      {name: "TypeScript", icon: '/public/skills/typescript.svg' },
+      {name: "Vite", icon: '/public/skills/vite.svg' },
     ],
     print: [
       {name: 'Home', img:'https://user-images.githubusercontent.com/128244805/234773510-84a6d446-9b3e-4f3d-8b12-7378e02e55be.png'},
